791bff92f251592e7036dfcd70e9bbfc916f1806
João Beltramini <jvbeltra2011@gmail.com>
2019-08-18 04:02:40 -0300

    Removendo index desnecessário

--- index.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongodb').MongoClient;
-
-import * as config from './resources/config.json';
-import * as httpServer from './src/httpServer/HttpServer';
-
-var port = config.http.port;
-
-httpServer.createHttpServer(port);
-
